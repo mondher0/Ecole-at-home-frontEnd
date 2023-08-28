2aa0a558a4e00076175131c3a4627d67668cb2ab
mondher04000 <m_mameri@estin.dz>
2023-08-28 13:50:48 +0100

    adding notifications

--- src/Components/Notification/Notification.jsx
+++ src/Components/Notification/Notification.jsx
@@ -1,16 +1,112 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable no-unused-vars */
-import React from "react";
+import { useContext, useEffect, useState } from "react";
 import NotificationCard from "../NotificationCard/NotificationCard";
+import { GlobalContext } from "../../context/GlobalContext";
+import axiosInstance, { baseURl } from "../../utils/utils";
+import "../../css/loader.css";
 
 const Notification = () => {
+  const { userInfo } = useContext(GlobalContext);
+  console.log(userInfo);
+  const { role } = userInfo;
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [isEmpy, setIsEmpy] = useState(false);
+  const [notifications, setnotifications] = useState("");
+
+  // get notifications of the teacher
+  const getNotificationsForTeacher = async () => {
+    try {
+      setIsEmpy(false);
+      setIsError(false);
+      setIsLoading(true);
+      const response = await axiosInstance.get(`${baseURl}/notification/prof`);
+      console.log(response);
+      setnotifications(response?.data);
+      if (response?.data?.length === 0) {
+        setIsEmpy(true);
+      }
+      setIsLoading(false);
+    } catch (error) {
+      setIsLoading(false);
+      setIsError(true);
+      console.log(error);
+    }
+  };
+
+  // get notifications of the student or parent
+  const getNotificationsForStudentOrParent = async () => {
+    try {
+      setIsEmpy(false);
+      setIsError(false);
+      setIsLoading(true);
+      const response = await axiosInstance.get(`${baseURl}/notification/user`);
+      console.log(response);
+      setnotifications(response?.data);
+      setIsLoading(false);
+    } catch (error) {
+      setIsLoading(false);
+      setIsError(true);
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    if (role === "teacher") {
+      getNotificationsForTeacher();
+    } else {
+      getNotificationsForStudentOrParent();
+    }
+  }, []);
   return (
     <div className="notification_page">
       <h3 className="main_title">Mes nouvelles</h3>
-      <NotificationCard />
-      <NotificationCard />
-      <NotificationCard />
-      <NotificationCard />
-      <NotificationCard />
+      {
+        // if there is no notifications
+        isEmpy && (
+          <div className="empty_container">
+            <h3
+              style={{
+                textAlign: "center",
+                fontSize: "1.5rem",
+              }}
+            >
+              Vous n{"'"}avez pas de nouvelles
+            </h3>
+          </div>
+        )
+      }
+      {
+        // if there is an error
+        isError && (
+          <div className="empty_container">
+            <h3
+              style={{
+                textAlign: "center",
+                fontSize: "1.5rem",
+              }}
+            >
+              Une erreur est survenue
+            </h3>
+          </div>
+        )
+      }
+      {isLoading ? (
+        <div className="spinner-container">
+          <div className="loading-spinner"></div>
+        </div>
+      ) : (
+        <div className="notification_container">
+          {notifications &&
+            notifications?.map((notification) => (
+              <NotificationCard
+                notification={notification}
+                key={notification.id}
+              />
+            ))}
+        </div>
+      )}
     </div>
   );
 };
--- src/Components/NotificationCard/NotificationCard.jsx
+++ src/Components/NotificationCard/NotificationCard.jsx
@@ -1,16 +1,19 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 import React from "react";
 
-const NotificationCard = () => {
+const NotificationCard = ({ notification }) => {
+  const { createdAt } = notification;
+  console.log(createdAt);
+  const inputDate = new Date(createdAt);
+  const options = { month: "short", day: "numeric" };
+  const formattedDate = inputDate.toLocaleDateString("en-US", options);
   return (
     <div className="the_notification_card">
-      <h3 className="notivication_time">Jan 22</h3>
+      <h3 className="notivication_time">{formattedDate}</h3>
       <div className="notification_text">
-        <h2>Lorem ipsum</h2>
-        <p>
-          Iikib dlir dolor sit amet, consectetur adipiscing elit. In sit arcu
-          aliquet ut dui egestas.
-        </p>
+        <h2>{notification?.id}</h2>
+        <p>{notification?.text}</p>
       </div>
     </div>
   );
